d6f440d1069f7b0bdf587b1e6705d168612ad8bc
Leonard Martin <leonard.martin@gmail.com>
2021-11-26 22:20:01 +0000

    Update About me content

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -12,6 +12,11 @@ export default function Portfolio() {
       </div>
 
       <p>I shoot a variety of sports including rugby, cricket, cycling, and swimming, but the majority of my work is around athletics.</p>
+
+      <p>My work has appeared in print in magazines such as Women's Running and Outdoor Swimmer, as well as many local newspapers in the London area including Ham&High, Hackney Gazette and Islington Tribune.</p>
+
+      <p>I am a strong believer in promoting women's sport, in particular in non-traditional sports such as cricket and rugby.</p>
+
       <p>When I'm not taking photos I also run with London Heathside Athletics Club, focussing on ultramarathon and mountain races, although I have been known to race on the roads occasionally too.</p>
 
       <div className="-mx-6 mb-12 grid grid-cols-1 sm:grid-cols-2 gap-1">
@@ -25,12 +30,18 @@ export default function Portfolio() {
 
       <p>I have taken photos for a range of races from league and championship XC races, through track and field athletics, to 100 mile ultramarathons.</p>
 
+      <p>I have worked with a number race organisers including XNRG, Centurion Running, and SVP50/100.</p>
+
       <div className="-mx-6 mb-12 grid grid-cols-1 sm:grid-cols-2 gap-1">
         <div className="relative aspect-w-3 aspect-h-2"><Image src="/images/sdw.jpg" layout="fill" /></div>
         <div className="relative aspect-w-3 aspect-h-2"><Image src="/images/cts.jpg" layout="fill" /></div>
         <div className="relative aspect-w-3 aspect-h-2"><Image src="/images/fast-friday.jpg" layout="fill" /></div>
         <div className="relative aspect-w-3 aspect-h-2"><Image src="/images/xnrg.jpg" layout="fill" /></div>
       </div>
+
+      <h2>Contact</h2>
+
+      <p>You can email me on <a href="mailto:info@leonardmartin.photo">info@leonardmartin.photo</a></p>
 
       <h2>Follow Me</h2>
 
