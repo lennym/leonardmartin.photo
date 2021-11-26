2515e44337fd00449e19d6713908789ab2256f31
Leonard Martin <leonard.martin@gmail.com>
2021-11-26 11:39:30 +0000

    Add tags to gallery preview

--- components/gallery-preview.js
+++ components/gallery-preview.js
@@ -3,14 +3,21 @@ import Image from './image'
 
 import TimeStamp from './date'
 
-export default function GalleryPreview({ className, cover, title, id, updated }) {
+export default function GalleryPreview({ className, cover, title, id, updated, tags = [] }) {
   return (
     <div className={`${className} bg-white shadow-md rounded-md overflow-hidden relative`}>
       <Link href={`/galleries/${id}`}>
         <a className="">
           <Image src={`/preview/${id}/${cover}?size=medium`} />
           <p className="pt-4 px-6 mb-0">{ title }</p>
-          <p className="pb-4 px-6 mb-0 text-gray-400 text-base"><TimeStamp timestamp={updated} /></p>
+          <p className="pb-4 px-6 mb-0 text-gray-400 text-base">
+            <TimeStamp timestamp={updated} />
+            {
+              tags.map(tag => {
+                return <span key={tag} className="inline-block bg-gray-100 rounded-md px-2 mx-1">{tag}</span>
+              })
+            }
+          </p>
         </a>
       </Link>
     </div>
