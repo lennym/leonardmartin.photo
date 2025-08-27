977afc581adc24c03e3b39d01095538380ab5c8e
Leonard Martin <leonard.martin@gmail.com>
2025-08-27 23:02:47 +0100

    Log error on database connection falure

--- pages/api/checkout/index.js
+++ pages/api/checkout/index.js
@@ -1,31 +1,36 @@
-import { get } from 'lodash'
-import Stripe from 'stripe'
+import { get } from 'lodash';
+import Stripe from 'stripe';
 
-import { database } from '../../../knex'
-import { withSessionRoute } from '../../../lib/session'
-import sendConfirmation from '../../../lib/send-confirmation'
+import { database } from '../../../knex';
+import sendConfirmation from '../../../lib/send-confirmation';
+import { withSessionRoute } from '../../../lib/session';
 
-const stripe = Stripe(process.env.STRIPE_KEY_SECRET, { apiVersion: '2020-08-27' })
+const stripe = Stripe(process.env.STRIPE_KEY_SECRET, {
+  apiVersion: '2020-08-27',
+});
 
 export default withSessionRoute(async function Checkout(req, res) {
-
   if (req.method === 'POST') {
-    const knex = await database()
-    const basket = req.session.basket || []
-
     try {
-      const { amount, email } = req.body
-      const order = await knex('orders').insert({ email, amount }).returning('*')
-      const images = await knex('order_images').insert(basket.map(image_id => ({ image_id, order_id: order[0].id })))
-      req.session.basket = []
-      await req.session.save()
+      const knex = await database();
+      const basket = req.session.basket || [];
+      const { amount, email } = req.body;
+      const order = await knex('orders')
+        .insert({ email, amount })
+        .returning('*');
+      const images = await knex('order_images').insert(
+        basket.map((image_id) => ({ image_id, order_id: order[0].id }))
+      );
+      req.session.basket = [];
+      await req.session.save();
       if (email) {
-        await sendConfirmation(email, order[0].id)
+        await sendConfirmation(email, order[0].id);
       }
-      res.status(200).json({ ...order[0], images: basket })
+      res.status(200).json({ ...order[0], images: basket });
     } catch (error) {
-      res.status(get(error, 'raw.statusCode') || 500).json({ error: error.message })
+      res
+        .status(get(error, 'raw.statusCode') || 500)
+        .json({ error: error.message });
     }
   }
-
-})
+});
